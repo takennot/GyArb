3340dc8faa3ba6565caa6c714f4ebaaa7cae73eb
SagaLD <sagaliljenrothdickman@gmail.com>
2020-12-28 20:50:19 +0100

    algorithm file

--- php/algorithm.php
+++ php/algorithm.php
@@ -1,17 +1,17 @@
 <?php 
-	$servername = "46.59.18.164"; #46.59.18.164 #localhost
-	$username = "rus";
-	$password = "Computer123";
-	$dbname = "computerparts";
+    $servername = "46.59.18.164:3306"; #46.59.18.164 #localhost
+    $username = "ruski";
+    $password = "hund";
+    $dbname = "computerparts";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-  		die("Connection failed: " . $conn->connect_error);
-	}
-	else{
-		echo "database connected! :D"
-	}
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+    }
+    else{
+        echo "database connected! :D";
+    }
 
 ?>
