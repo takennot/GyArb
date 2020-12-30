0695bab4f747dfbf530daa5796fb3989e65d927e
SagaLD <sagaliljenrothdickman@gmail.com>
2020-12-30 12:12:25 +0100

    html,js and css update for getcomputer page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ $(document).ready(function(){
     
   });
 });
+
+function getComputer(){
+	var budget = document.getElementById("inputBudget").value;
+	alert("Budget: " + budget);
+
+	var RGB = document.getElementById("checkboxRGB").checked;
+	alert("RGB: " + RGB);
+
+	var mainUse;
+	if(document.getElementById("checkboxGaming").checked){
+		mainUse = 'gaming';
+	}
+	else if(document.getElementById("checkboxOfficeHome").checked){
+		mainUse = 'officeHome';
+	}
+	else if(document.getElementById("checkboxWorkRender").checked){
+		mainUse = 'workRendering';
+	}
+	else{
+
+	}
+	alert("Main use: " + mainUse);
+
+}
